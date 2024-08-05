f387d762e68ebb249e058a7ca64706f68bc3c6f0
Yanun.Y <yanun.y@intteam.net>
2024-08-05 11:04:51 +0800

    fix: add direction into image

--- image_spirit_sheet.go
+++ image_spirit_sheet.go
@@ -1,5 +1,23 @@
 package ebitenpkg
 
-import sysimage "image"
+import (
+	sysimage "image"
 
-type SpiritSheetOption func(moveX, moveY float64, imgWidth, imgHeight int, side, currentSecond int) sysimage.Rectangle
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type Direction Align
+
+type SpiritSheetOption func(direction Direction, imgWidth, imgHeight int, currentSecond int) sysimage.Rectangle
+
+func (opt *SpiritSheetOption) subImage(img *ebiten.Image, direction Direction, currentSecond int) *ebiten.Image {
+	if opt == nil {
+		return img
+	}
+
+	fn := *opt
+
+	rect := fn(direction, img.Bounds().Dx(), img.Bounds().Dy(), currentSecond)
+
+	return img.SubImage(rect).(*ebiten.Image)
+}
